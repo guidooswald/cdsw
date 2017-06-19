012c6d3169ece7bf5d362d2c5053504c5510acd2
Guido Oswald <guido.oswald@gmail.com>
2017-06-19 11:12:46 +0200

    Update pyspark.py

--- pyspark.py
+++ pyspark.py
@@ -116,3 +116,33 @@ for each in teenagers.collect():
     print(each[0])
 
 spark.stop()
+
+
+
+# # Word counts
+# 
+# This example shows how to count the occurrences of each word in a text file.
+
+from __future__ import print_function
+import sys, re
+from operator import add
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+    .builder\
+    .appName("PythonWordCount")\
+    .getOrCreate()
+
+# Add the data file to hdfs.
+!hdfs dfs -put resources/cgroup-v2.txt /tmp
+
+lines = spark.read.text("/tmp/cgroup-v2.txt").rdd.map(lambda r: r[0])
+counts = lines.flatMap(lambda x: x.split(' ')) \
+              .map(lambda x: (x, 1)) \
+              .reduceByKey(add) \
+              .sortBy(lambda x: x[1], False)
+output = counts.collect()
+for (word, count) in output:
+    print("%s: %i" % (word, count))
+
+spark.stop()
